fd2361e15dd61d1f88d31f1f5c6259e9204debd8
Malte Muench <mamu@stablerock.de>
2024-05-19 10:54:19 +0200

    remove dependency on crypted contents of the repo
    Signed-off-by: Malte Muench <mamu@stablerock.de>

--- deploy_flux/deploy_flux.sh
+++ deploy_flux/deploy_flux.sh
@@ -3,7 +3,7 @@
 kubectl apply -f ../flux/flux-system/gotk-components.yaml
 
 # Apply GPG private-key (used to decrypt secrets)
-gpg -d cluster-private-key.gpg | kubectl apply -f -
+# gpg -d cluster-private-key.gpg | kubectl apply -f -
 
 # Apply initial flux-config (gitrepo and root-kustomization)
 kubectl apply  -f ../flux/flux-system/root-ks.yaml
