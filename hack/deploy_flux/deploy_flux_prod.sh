3bc98089231fb7a26d6420a3efd598f056709531
Malte Münch <muench@b1-systems.de>
2024-05-28 16:09:00 +0200

    prod and dev via script (#11)
    * New script for prod cluster
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    ---------
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    Co-authored-by: Paul Hildebrandt <hildebrandt@b1-systems.de>

---
+++ hack/deploy_flux/deploy_flux_prod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Create namespace for the CSO
+kubectl create namespace cso-system
+
+# Install flux and flux-system namespace
+kubectl apply -f ../../flux/installation/flux-installation.yaml
+
+# Apply GPG private-key (used to decrypt secrets)
+gpg -d cluster-private-key.gpg | kubectl apply -f -
+
+# Apply initial flux-config (gitrepo and root-kustomization)
+kubectl apply  -f ../../flux/config/flux-config-ks.yaml
+
+# Apply additional flux-config (prod-only)
+kubectl apply  -f ../../prod/flux/config/prod-flux-config-ks.yaml
