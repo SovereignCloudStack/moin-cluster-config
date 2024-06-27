cbce67eef5bcaf09105293696b552723d8b0eae3
Malte Münch <muench@b1-systems.de>
2024-06-27 13:04:45 +0200

    main volatile (#33)
    * choose temporary branch main-volatile
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Set allowed k8s versions to current image list on gx-scs
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Polish kyverno config
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Adjust kyvernos permissions to the same level it wants to grant
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Fix syntax
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Removes playground namespaces
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Adds gitrepo openstack-csp-helper
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Adds oc-csp-helper for playground0
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Adds releases and secrets for playgrounds 1-9
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Adds backup config
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Adds RBAC to retrieve clusterclasses publicy and unauthenticated
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Add kubeconfig and docs (#27)
    
    Signed-off-by: Paul Hildebrandt <hildebrandt@b1-systems.de>
    
    * capi-visualizer docs (#28)
    
    Signed-off-by: Paul Hildebrandt <hildebrandt@b1-systems.de>
    
    * chore(rbac): remove playground1 specific rbac
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * chore(rbac): replace oidc prefixes
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * fix(k8s config): remove namespaces.yaml
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * fix(kyverno): fix oidc prefix in rbac
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * fix(clusterrole): add infrastructure.cluster
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * feat(deployments): add cspo
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * feat(deployments): add cspo
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * feat(dev deployment): add cspo
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * feat(dev deployment): add cspo
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * chore(prod deployment): not needed to create cso namespace in prod
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * CSPO secret
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * fix(prod ks): add cspo-config
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * fix(cspo): remove secret from manifest
    
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    
    * Adds cluster-gen
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Fix syntax
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Fix syntax
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Fix syntax
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Fix syntax
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Fix syntax
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    * Prepartion for merge to main, set reconciliation branch to main
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    
    ---------
    
    Signed-off-by: Malte Münch <muench@b1-systems.de>
    Signed-off-by: Paul Hildebrandt <hildebrandt@b1-systems.de>
    Signed-off-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>
    Co-authored-by: paul <hildebrandt@b1-systems.de>
    Co-authored-by: Jan Schoone <6106846+jschoone@users.noreply.github.com>

--- hack/deploy_flux/deploy_flux_dev.sh
+++ hack/deploy_flux/deploy_flux_dev.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 # Create namespace for the CSO
 kubectl create namespace cso-system
+kubectl create namespace cspo-system
 
 # Create the config for the CSO
 kubectl create secret generic cso-cluster-stack-variables -n cso-system \
-    --from-literal=git-access-token=$GITHUB_TOKEN \
-    --from-literal=git-org-name=SovereignCloudStack \
-    --from-literal=git-provider=github \
-    --from-literal=git-repo-name=cluster-stacks
+	--from-literal=git-access-token=$GITHUB_TOKEN \
+	--from-literal=git-org-name=SovereignCloudStack \
+	--from-literal=git-provider=github \
+	--from-literal=git-repo-name=cluster-stacks
+
+# Create namespace for the CSPO
+kubectl create namespace cspo-system
+
+# Create the config for the CSPO
+kubectl create secret generic cspo-cluster-stack-variables -n cspo-system \
+	--from-literal=git-access-token=$GITHUB_TOKEN \
+	--from-literal=git-org-name=SovereignCloudStack \
+	--from-literal=git-provider=github \
+	--from-literal=git-repo-name=cluster-stacks
 
 # Install flux and flux-system namespace
 kubectl apply -f ../../flux/installation/flux-installation.yaml
 
 # Apply initial flux-config (gitrepo and root-kustomization)
-kubectl apply  -f ../../flux/config/flux-config-ks.yaml
+kubectl apply -f ../../flux/config/flux-config-ks.yaml
